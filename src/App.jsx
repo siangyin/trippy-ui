5516b1c9fc99c496a3e3fb0fb122f004ae317db3
SiangYin <siangyin.work@gmail.com>
2026-04-20 23:57:51 +0800

    add default style, and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
+
+import NavBar from "./components/NavBar"
+import AboutUs from "./components/AboutUs"
+import Trip from "./components/Trip"
+import ContactForm from "./components/ContactForm"
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="App">
+      <NavBar />
+      App
+      <Routes>
+        {/* <Route path="/" element={<Hero />} />
+        <Route path="/destination" element={<Destination />} /> */}
+        <Route path="/tours" element={<Trip />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactForm />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
